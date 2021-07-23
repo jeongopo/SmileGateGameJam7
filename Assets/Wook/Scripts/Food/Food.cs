0d21b22c602cf672b725088b4ed798bbe4c97ad5
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-07-24 02:04:51 +0900

    Food enum

--- Assets/Wook/Scripts/Food/Food.cs
+++ Assets/Wook/Scripts/Food/Food.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    public enum FoodType
+    {
+        Drink,
+        Ingredients_Main,
+        Ingredients_Sub
+    }
 [CreateAssetMenu(fileName = "New Food", menuName = "New Food/Food")]
 public class Food : ScriptableObject
 {
@@ -13,9 +19,5 @@ public class Food : ScriptableObject
 
     public Food[] Ingredients;  //재료 (재료 개수에 따라 랭크 1개면 1성)
 
-    public enum FoodType
-    {
-        Drink,
-        Ingredients
-    }
+
 }
